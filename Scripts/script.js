82ce714b25486ecd11a0aa6940deede2594738a6
Ignacio Avecilla <iavecilla@fi.uba.ar>
2021-04-18 23:16:06 -0300

    Dark Theme persistente entre distintos HTML

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,14 +1,23 @@
 const body = document.getElementsByTagName("BODY")[0];
 const btn = document.querySelector(".btnTheme");
 
+function save (strKey,strValue) {
+    localStorage.setItem(strKey, strValue);
+}
+
 btn.onclick = () => {
-    if (body.classList.contains("dark_theme")){
-        body.classList.remove("dark_theme");
+    body.classList.toggle("dark_theme");
+    if (localStorage["class"]) {
+        localStorage.removeItem("class");
     } else {
-        body.classList.add("dark_theme");
+        save("class","dark_theme");
     }
 }
 
-
+if (!localStorage.getItem("class")) {
+    body.classList.remove("dark_theme");
+} else {
+    body.classList.add("dark_theme");
+}
 
 
